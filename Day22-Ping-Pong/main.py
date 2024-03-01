d6a996e9efe0349b2247f26bcaf9aef27db38aae
user_1-at-010531575764 <vinay588325@gmail.com>
2024-03-01 15:27:12 +0530

    Pingpong added

--- Day22-Ping-Pong/main.py
+++ Day22-Ping-Pong/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from score_board import Score
 import time
 
 main_screen = Screen()
@@ -12,12 +13,18 @@ main_screen.tracer(0)
 paddle_1 = Paddle((350,0))
 paddle_2 = Paddle((-350,0))
 ball = Ball()
+score_board = Score()
+
+# def quit_game():
+#     print("q")
+#     game_on = False
 
 main_screen.listen()
 main_screen.onkey(key="Up", fun=paddle_1.move_up)
 main_screen.onkey(key="Down", fun=paddle_1.move_down)
 main_screen.onkey(key="w", fun=paddle_2.move_up)
 main_screen.onkey(key="s", fun=paddle_2.move_down)
+#main_screen.onkey(key="q", fun=quit_game)
 
 
 game_on = True
@@ -29,10 +36,16 @@ while game_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    if ball.distance(paddle_1) < 60 and ball.xcor() > 320 or ball.distance(paddle_2) < 50 and ball.xcor() < -320:
+    if (ball.distance(paddle_1) < 60 and ball.xcor() > 320) or (ball.distance(paddle_2) < 50 and ball.xcor()) < -320:
         ball.bounce_x() 
 
     if ball.xcor() > 350 or ball.xcor() < -350:
         #Score add and reset the ball
+        if ball.xcor() > 350:
+            score_board.l_point()
+        else:
+            score_board.r_point()
         ball.reset_position()
+
+    #main_screen.onkey(key="q", fun=quit_game)
 main_screen.exitonclick()
